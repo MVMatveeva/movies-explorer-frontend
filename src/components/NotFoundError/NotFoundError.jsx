3b848620cd57bab64397fa1625dbe86bfb3f37f4
Maria Matveeva <marikadenisova@yandex.ru>
2024-04-13 15:24:09 +0300

    Исправление ошибок: кнопка назад

--- src/components/NotFoundError/NotFoundError.jsx
+++ src/components/NotFoundError/NotFoundError.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function NotFoundError() {
+  const navigate = useNavigate();
+
+  const returnBack = () => {
+    navigate(-1);
+  };
 
   return (
     <section className="NotFoundError">
       <h2 className="NotFoundError__heading">404</h2>
       <p className="NotFoundError__text">Страница не найдена</p>
-      <button className="NotFoundError__link">Назад</button>
+      <button className="NotFoundError__link" onClick={returnBack}>Назад</button>
     </section>
   );
 }
